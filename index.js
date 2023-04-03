dc70acd74d6360d10cdf2e2d800a7b2da2c18e11
Wesakania <wesakaniadavis@gmail.com>
2023-04-03 19:44:12 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+/*ReactDOM.render(
+    <ul>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Our products</li>
+        <li>Branches</li>
+    </ul>,
+    document.getElementById("root")
+)
+
+const nameList = document.createElement("nameList")
+nameList.textContent = "This is the list of the entire form one cohort"
+
+nameList.className = "header"
+
+document.getElementById("root").append(nameList)
+
+
+ ReactDOM.render(<p className = "header">This is the <strong>entire list</strong> of form one cohort</p>,
+  document.getElementById("root")
+ )
+ 
+
+ const h1 = document.createElement("h1")
+ h1.textContent = "How is everyone here?"
+ h1.className = "Header"
+
+ console.log(h1)
+
+ const element = <h1 className = "header">This is JSX</h1>
+
+ console.log(element)
+
+ReactDOM.render(element, document.getElementById("root"))
+
+const page = (
+    <div>
+        <h2 className = 'header'>Hello World, this is how we do it!</h2>
+        <h4>When are you enrolling for coding classes?</h4>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
+
+
+
+ReactDOM.render(
+    <div>
+        <h1 className = "header">Hey guys the classes have begun</h1>
+        <h3>Are you not aware?</h3>
+    </div>,
+    document.getElementById("root")
+)
+*/
+
+const navBar = (
+    <div>
+        <h1 className = "header">Welcome to My BISTRO</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Products</li>
+            <li>cContacts</li>
+        </ul>
+    </div>
+)
+    ReactDOM.render(navBar, document.getElementById("root"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
